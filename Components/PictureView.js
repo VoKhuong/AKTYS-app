69b30f276746ed5c3d0d41c3611f1a123f524571
unknown <abdelkader.zerouali@gmail.com>
2019-06-19 14:08:16 +0200

    Added the futur view to take picture

---
+++ Components/PictureView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, ImageBackground, Image } from 'react-native'
+
+class PictureView extends React.Component{
+
+}
+
+
+export default PictureView
