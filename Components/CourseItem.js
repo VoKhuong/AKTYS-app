75982a971e73294366677bee4577b86301aaa8e1
Vo Lam Nhat Khuong <volamnhatkhuong@gmail.com>
2019-06-19 12:16:35 +0200

    courseView + item definition c bo <3

--- Components/CourseItem.js
+++ Components/CourseItem.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, Image, View, Text } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 
 class CourseItem extends React.Component {
@@ -7,16 +7,46 @@ class CourseItem extends React.Component {
     render(){
         const course = this.props.course
         // traitement
-        colorArray = ['#00F260', '#0575E6']
+        if (course.subject == 'Informatique') {
+            img = require('../assets/infoIcon.png')
+        } else if (course.subject == 'Mathématiques') {
+            img = require('../assets/mathIcon.png')
+        } else if (course.subject == 'Droit') {
+            img = require('../assets/lawIcon.png')
+        } else {
+            img = require('../assets/courseIcon.png')
+        }
+
+        if (course.teacher.id == '1') {
+            colorArray = ['#fc4a1a', '#f7b733'] // yellowOrange
+        } else {
+            colorArray = ['#00F260', '#0575E6'] // greenBlue
+        }
+
         // fin traitement
         return (
-            <LinearGradient style = { styles.container }
-            elevation = { 12 }
-            start = {[0, 0]}
-            end = {[1, 1]}
-            colors={colorArray}
-            >
-            </LinearGradient>
+            <View style = { styles.container }>
+                <View style = { styles.content } >
+                    <View style = { styles.iconContainer }>
+                        <Image style = { styles.icon } source={ img } />
+                    </View>
+                    <View style = { styles.info }>
+                        <Text style = { styles.info1 } >{ course.subject }</Text>
+                        <Text style = { styles.info2 } >9:00 - 12:30</Text>
+                        <Text style = { styles.info2 } >{ course.teacher.firstName } { course.teacher.lastName.toUpperCase() }</Text>
+                    </View>
+                    <View style = { styles.nbStudents }>
+                        <Text style = { styles.textFirst } >{ course.present }</Text>
+                        <Text style = { styles.textSecond }>/ { course.expected }</Text>
+                    </View>
+                </View>
+                <LinearGradient
+                    style = { styles.footer }
+                    start = {[0, 0]}
+                    end = {[1, 1]}
+                    colors={colorArray}
+                />
+            </View>
         )
     }
 
@@ -24,9 +54,11 @@ class CourseItem extends React.Component {
 
 const styles = StyleSheet.create({
     container : {
-        height : 80,
+        height : 100,
         width: 'auto',
-        marginBottom: 20,
+        backgroundColor: 'white',
+        marginTop: 10,
+        marginBottom: 10,
         marginRight: 20,
         marginLeft: 20,
         borderRadius: 3,
@@ -36,7 +68,62 @@ const styles = StyleSheet.create({
             height: 2
         },
         shadowRadius: 3,
-        shadowOpacity: 1
+        shadowOpacity: 1,
+        elevation : 6
+    },
+    content: {
+        flex: 8,
+        flexDirection: 'row'
+    },
+    iconContainer: {
+        flex: 1,
+        borderTopLeftRadius: 3,
+        padding: 6
+    },
+    icon: {
+        resizeMode: 'contain',
+        height: '100%',
+        width: 'auto'
+    },
+    info: {
+        flex: 2
+    },
+    info1: {
+        flex: 2,
+        fontSize: 22,
+        padding: 6,
+        color: '#231f20'
+    },
+    info2: {
+        flex: 1,
+        paddingBottom: 4,
+        paddingLeft: 6,
+        paddingRight: 6,
+        fontSize: 12,
+        color: '#878485'
+    },
+    nbStudents: {
+        flex: 1,
+        borderTopRightRadius: 3
+    },
+    textFirst: {
+        flex: 3,
+        fontSize: 38,
+        paddingLeft: 10,
+        color: '#09bc8a'
+    },
+    textSecond: {
+        flex: 2,
+        fontSize: 26,
+        textAlign: 'right',
+        paddingRight: 10,
+        paddingBottom: 8,
+        color: '#4CCEA9'
+    },
+    footer: {
+        flex: 2,
+        borderBottomLeftRadius: 3,
+        borderBottomRightRadius: 3
     }
 })
 
--- Components/HistoryView.js
+++ Components/HistoryView.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View, StyleSheet, FlatList, Text } from 'react-native'
+import { View, StyleSheet, FlatList, Image } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 import CourseItem from './CourseItem'
 
 class HistoryView extends React.Component {
@@ -7,9 +8,16 @@ class HistoryView extends React.Component {
     render(){
         return (
             <View style = { styles.container }>
+                <LinearGradient style = { styles.header }
+                    start = {[0, 0]}
+                    end = {[1, 1]}
+                    colors={['#00F260', '#0575E6']}
+                >
+                    <Image style = { styles.iconImg } source={require('../assets/historyBigIcon.png')} />
+                </LinearGradient>
                 <FlatList
-                    data={[{key: 'a'}, {key: 'b'}]}
-                    renderItem={ ({item}) => <CourseItem film = { item } /> }
+                    data={[{subject: 'Informatique', teacher: {firstName: 'Jean-Jacques', lastName: 'gogo', id: '1'}, present: 12, expected: 20}, {subject: 'Mathématiques', teacher: {firstName: 'Jean-Jacques', lastName: 'gogo', id: '2'}, present: 12, expected: 20}, {subject: 'Droit', teacher: {firstName: 'Jean-Jacques', lastName: 'gogo', id: '3'}, present: 12, expected: 20}, {subject: 'dunno', teacher: {firstName: 'Jean-Jacques', lastName: 'gogo', id: '4'}, present: 12, expected: 20}]}
+                    renderItem={ ({item}) => <CourseItem course = { item } /> }
                 />
             </View>
         )
@@ -21,6 +29,16 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#d8ddef',
         flex: 1
+    },
+    header: {
+        height: '20%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    iconImg: {
+        height: '60%',
+        resizeMode: 'contain',
+        marginTop: '5%'
     }
 })
 
